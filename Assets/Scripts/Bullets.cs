cb4bca5280d6fc903be3f59c745cfcdd0d86ec69
Lee-Mifsud01 <128689240+Lee-Mifsud01@users.noreply.github.com>
2024-05-02 23:20:58 +0200

    Added some basic ui

--- Assets/Scripts/Bullets.cs
+++ Assets/Scripts/Bullets.cs
@@ -6,6 +6,8 @@ public class Bullets : MonoBehaviour
 {
    public GameObject bullet;
    public Transform bulletSpawn;
+   public Transform bulletSpawn2;
+   public Transform bulletSpawn3;
    public float bulletSpeed = 20;
 
    Vector2 lookDirection;
@@ -18,8 +20,14 @@ public class Bullets : MonoBehaviour
       if (Input.GetMouseButtonDown(0))
       {
          GameObject bulletClone = Instantiate(bullet, bulletSpawn.position, Quaternion.identity);
+         GameObject bulletClone1 = Instantiate(bullet, bulletSpawn2.position, Quaternion.identity);
+         GameObject bulletClone2 = Instantiate(bullet, bulletSpawn3.position, Quaternion.identity);
+
 
          bulletClone.GetComponent<Rigidbody2D>().velocity = lookDirection.normalized * bulletSpeed;
+         bulletClone1.GetComponent<Rigidbody2D>().velocity = lookDirection.normalized * bulletSpeed;
+         bulletClone2.GetComponent<Rigidbody2D>().velocity = lookDirection.normalized * bulletSpeed;
+
       }
    }
 }
